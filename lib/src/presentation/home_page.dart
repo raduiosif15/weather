c8c1590c072c3e59282c4cf1d391ea0f5d4af047
Iosif Radu <raduiosif15@yahoo.com>
2021-07-10 18:54:27 +0300

    First basic presentation for location.

--- lib/src/presentation/home_page.dart
+++ lib/src/presentation/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:weather/src/container/location_container.dart';
+import 'package:weather/src/models/location.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,6 +10,24 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Location'),
+      ),
+      body: LocationContainer(
+        builder: (BuildContext context, Location? location) {
+          if (location == null) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return Column(
+            children: <Widget>[
+              Text('Tara: ${location.country}'),
+              Text('Oras: ${location.city}'),
+              Text('Latitudine: ${location.lat}'),
+              Text('Longitudine: ${location.lon}'),
+            ],
+          );
+        },
       ),
     );
   }
--- lib/src/reducer/reducer.dart
+++ lib/src/reducer/reducer.dart
@@ -1,9 +1,11 @@
 import 'package:redux/redux.dart';
 import 'package:weather/src/actions/get_location.dart';
 import 'package:weather/src/models/app_state.dart';
-import 'package:weather/src/models/location.dart';
 
 Reducer<AppState> reducer = combineReducers(<Reducer<AppState>>[
+  (AppState state, dynamic action) {
+    return state;
+  },
   TypedReducer<AppState, GetLocation>(_getLocation),
   TypedReducer<AppState, GetLocationSuccessful>(_getLocationSuccessful),
   TypedReducer<AppState, GetLocationError>(_getLocationError),
@@ -15,7 +17,7 @@ AppState _getLocation(AppState state, GetLocation action) {
 
 AppState _getLocationSuccessful(AppState state, GetLocationSuccessful action) {
   return state.rebuild((AppStateBuilder b) {
-    b.location = action.location as LocationBuilder?;
+    b.location.replace(action.location);
   });
 }
 
